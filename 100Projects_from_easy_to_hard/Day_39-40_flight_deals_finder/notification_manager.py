7be5110ba34a2b1f752f2315b15b96188161871e
Dhruv kumawat <93523569+dhruvdk3@users.noreply.github.com>
2023-08-23 22:06:23 +0530

    Update notification_manager.py

--- 100Projects_from_easy_to_hard/Day_39-40_flight_deals_finder/notification_manager.py
+++ 100Projects_from_easy_to_hard/Day_39-40_flight_deals_finder/notification_manager.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
-twlio_number = "+13137658019"
-auth_token = "1b18414b6024ede69ddcfc53b342dc91"
-account_sid = "ACc25a1abbacc1396e71f667b020a5ad17"
+twlio_number = "teilo number"
+auth_token = "auth token"
+account_sid = "sid"
 class NotificationManager:
     def __intit__(self):
         self.client = Client(account_sid, auth_token)
@@ -10,7 +10,7 @@ class NotificationManager:
         message = self.client.messages.create(
             body=message,
             from_=twlio_number,
-            to="+918788421683",
+            to="your number",
         )
         # Prints if successfully sent.
         print(message.sid)
